b02470d54d4039678b7382e2b7fbcc12e4f6afe6
espritf <espritf@users.noreply.github.com>
2023-12-10 09:55:36 +0200

    use clap for reading cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Fetch data from given url
+    Fetch { url: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Fetch { url } => {
+            println!("get feed from: {url:?}");
+        }
+    }
 }
