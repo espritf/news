48753bec9ccc42afec6fef73d0f42c1fbd083f1f
espritf <espritf@users.noreply.github.com>
2023-12-10 10:59:48 +0200

    fetch feed from argument

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{Parser, Subcommand};
+use anyhow::Result;
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -13,12 +14,21 @@ enum Commands {
     Fetch { url: String },
 }
 
+
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
         Commands::Fetch { url } => {
             println!("get feed from: {url:?}");
+            let _ = fetch(url);
         }
     }
+}
+
+fn fetch(url: &String) -> Result<()> {
+    let res = reqwest::blocking::get(url)?.text()?;
+
+    println!("{res}");
+    Ok(())
 }
