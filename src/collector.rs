e95c198831d943652cf1b889a9307bb7f1e57f3e
esprit <espritf@users.noreply.github.com>
2023-12-14 09:03:38 +0200

    constructor for input::Channel

--- src/collector.rs
+++ src/collector.rs
@@ -1,18 +1,24 @@
+use crate::models::input::Channel;
 use anyhow::Result;
 use chrono::DateTime;
-use crate::models::input::Channel;
+
+impl Channel {
+    fn from(url: &String, ch: rss::Channel) -> Result<Self> {
+        let last_build_date =
+            DateTime::parse_from_rfc2822(ch.last_build_date().unwrap())?.naive_local();
+
+        Ok(Self {
+            title: ch.title,
+            link: url.clone(),
+            language: ch.language.unwrap(),
+            last_build_date,
+        })
+    }
+}
 
 pub fn fetch_channel(url: &String) -> Result<Channel> {
     let res = reqwest::blocking::get(url)?.text()?;
     let channel = rss::Channel::read_from(res.as_bytes())?;
-    let last_build_date =
-        DateTime::parse_from_rfc2822(channel.last_build_date().unwrap())?.naive_local();
 
-    Ok(Channel {
-        title: channel.title,
-        link: url.clone(),
-        language: channel.language.unwrap(),
-        last_build_date,
-    })
+    Channel::from(url, channel)
 }
-
