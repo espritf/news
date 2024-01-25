407a4d6e79c5285ab878bb28e9ff06c9ad369015
esprit <espritf@users.noreply.github.com>
2024-01-26 01:33:00 +0200

    get server address from env vars

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -13,13 +13,15 @@ async fn main() -> Result<()> {
     dotenv().ok();
 
     let uri = &env::var("DATABASE_URL")?;
+    let address = &env::var("SERVER_ADDR")?;
+
     let state = AppState::build(uri)?;
 
     let app = Router::new()
         .route("/", get(news_list))
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let listener = tokio::net::TcpListener::bind(address).await?;
     axum::serve(listener, app).await?;
 
     Ok(())
