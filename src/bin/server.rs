0c775cb35237cc97069ca196687fed0110c26635
esprit <espritf@users.noreply.github.com>
2024-01-26 02:25:28 +0200

    add tracing logs to server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -5,12 +5,15 @@ use axum::{
     routing::get,
     Router,
 };
+use tower_http::trace::TraceLayer;
 use news::news::news_list;
 use news::app::AppState;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+
+    tracing_subscriber::fmt::init();
 
     let uri = &env::var("DATABASE_URL")?;
     let address = &env::var("SERVER_ADDR")?;
@@ -19,9 +22,12 @@ async fn main() -> Result<()> {
 
     let app = Router::new()
         .route("/", get(news_list))
+        .layer(TraceLayer::new_for_http())
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind(address).await?;
+    tracing::info!("listening on {}", listener.local_addr()?);
+
     axum::serve(listener, app).await?;
 
     Ok(())
