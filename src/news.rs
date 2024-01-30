330324d4336e9c6e0db235350f65ac24497fc9a2
esprit <espritf@users.noreply.github.com>
2024-01-30 23:06:51 +0200

    handle errors properly

--- src/news.rs
+++ src/news.rs
@@ -1,6 +1,7 @@
 use crate::app::AppState;
 use crate::schema::news;
 use axum::extract::State;
+use axum::http::StatusCode;
 use axum::Json;
 use chrono::NaiveDateTime;
 use deadpool_diesel::sqlite::Pool;
@@ -17,7 +18,7 @@ pub struct News {
     pub_date: NaiveDateTime,
 }
 
-async fn get_news(pool: &Pool) -> Result<Vec<News>> {
+async fn get_news(pool: &Pool) -> Result<Vec<News>, Box<dyn std::error::Error>> {
     use crate::schema::news::dsl::*;
 
     let conn = pool.get().await?;
@@ -25,19 +26,21 @@ async fn get_news(pool: &Pool) -> Result<Vec<News>> {
         news.select(News::as_select())
             .load::<News>(c)
     })
-    .await
-    .unwrap()
-    .unwrap(); // TODO handle errors
+    .await??;
 
     Ok(res)
 }
 
 
 // get news list handler
-pub async fn news_list(State(state): State<AppState>) -> Json<Vec<News>> {
+pub async fn news_list(State(state): State<AppState>) -> Result<Json<Vec<News>>, StatusCode> {
 
     let pool = state.get_pool();
-    let news = get_news(pool).await.unwrap(); // TODO handle errors
-
-    Json(news)
+    match get_news(pool).await {
+        Ok(news) => Ok(Json(news)),
+        Err(e) => {
+            tracing::error!("Error ocurred: {}", e);
+            Err(StatusCode::INTERNAL_SERVER_ERROR)
+        }
+    }
 }
