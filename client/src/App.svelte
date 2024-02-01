f32b5bf20ec30941047df2a0bd15b08dd1854b30
esprit <espritf@users.noreply.github.com>
2024-02-01 22:54:44 +0200

    client: load and show news

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,8 +1,21 @@
 <script>
+    async function getUsers() {
+        const res  = await fetch('http://localhost:3000/');
+        const data = await res.json();
+        return data;
+    }
 </script>
 
 <main>
+    <h1>News</h1>
+    {#await getUsers()}
+        <p>Loading...</p>
+    {:then news}
+        {#each news as item}
+            <div>
+                <h4>{item.title}</h4>
+                <small>{new Date(item.pub_date).toUTCString()}</small>
+            </div>
+        {/each}
+    {/await}
 </main>
-
-<style>
-</style>
