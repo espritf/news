161644d915caaf2004c7e2e17aee2bfba677a1a8
esprit <espritf@users.noreply.github.com>
2024-02-24 03:07:15 +0200

    server: move structs into own module

--- server/src/news.rs
+++ server/src/news.rs
@@ -1,68 +1,69 @@
-use crate::app::AppState;
-use crate::schema::news;
-use anyhow::Result;
-use axum::extract::Path;
-use axum::extract::State;
-use axum::http::StatusCode;
-use axum::Json;
-use chrono::NaiveDateTime;
-use deadpool_diesel::sqlite::Pool;
-use diesel::AsExpression;
-use diesel::deserialize::{FromSql, FromSqlRow};
-use diesel::backend::Backend;
-use serde::{Deserialize, Serialize};
-use diesel::prelude::*;
-use diesel::serialize::{IsNull, Output, ToSql};
-use diesel::sql_types::Text;
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, FromSqlRow, AsExpression)]
-#[diesel(sql_type = Text)]
-pub struct Sources(Vec<String>);
+mod model {
+    use diesel::prelude::*;
+    use crate::schema::news;
+    use diesel::AsExpression;
+    use diesel::deserialize::{FromSql, FromSqlRow};
+    use diesel::backend::Backend;
+    use serde::{Deserialize, Serialize};
+    use diesel::serialize::{IsNull, Output, ToSql};
+    use diesel::sql_types::Text;
+    use chrono::NaiveDateTime;
 
-impl<DB> FromSql<Text, DB> for Sources
-where
-    DB: Backend,
-    String: FromSql<Text, DB>,
-{
-    fn from_sql(bytes: DB::RawValue<'_>) -> diesel::deserialize::Result<Self> {
-        let s = String::from_sql(bytes)?;
-        Ok(Self(serde_json::from_str(&s)?))
+    #[derive(Serialize, Deserialize, Debug, PartialEq, FromSqlRow, AsExpression)]
+    #[diesel(sql_type = Text)]
+    pub struct Sources(Vec<String>);
+
+    impl<DB> FromSql<Text, DB> for Sources
+        where
+            DB: Backend,
+            String: FromSql<Text, DB>,
+    {
+        fn from_sql(bytes: DB::RawValue<'_>) -> diesel::deserialize::Result<Self> {
+            let s = String::from_sql(bytes)?;
+            Ok(Self(serde_json::from_str(&s)?))
+        }
     }
-}
 
-// https://docs.rs/diesel/2.1.4/diesel/serialize/trait.ToSql.html#
-impl ToSql<Text, diesel::sqlite::Sqlite> for Sources
-    where
-        String: ToSql<Text, diesel::sqlite::Sqlite>,
-{
-    fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, diesel::sqlite::Sqlite>) -> diesel::serialize::Result {
-        let s = serde_json::to_string(&self.0)?;
-        out.set_value(s);
-        Ok(IsNull::No)
+    // https://docs.rs/diesel/2.1.4/diesel/serialize/trait.ToSql.html#
+    impl ToSql<Text, diesel::sqlite::Sqlite> for Sources
+        where
+            String: ToSql<Text, diesel::sqlite::Sqlite>,
+    {
+        fn to_sql<'b>(&'b self, out: &mut Output<'b, '_, diesel::sqlite::Sqlite>) -> diesel::serialize::Result {
+            let s = serde_json::to_string(&self.0)?;
+            out.set_value(s);
+            Ok(IsNull::No)
+        }
     }
-}
 
-#[derive(Serialize, Queryable, Selectable, Debug, PartialEq, Insertable)]
-#[diesel(table_name = news)]
-#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct News {
-    id: i32,
-    title: String,
-    pub_date: NaiveDateTime,
-    sources: Sources,
-}
+    #[derive(Serialize, Queryable, Selectable, Debug, PartialEq, Insertable)]
+    #[diesel(table_name = news)]
+    #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+    pub struct News {
+        id: i32,
+        title: String,
+        pub_date: NaiveDateTime,
+        sources: Sources,
+    }
 
-#[derive(Deserialize, Debug, PartialEq, Insertable)]
-#[diesel(table_name = news)]
-#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct NewsInput {
-    title: String,
-    pub_date: NaiveDateTime,
-    sources: Sources,
+    #[derive(Deserialize, Debug, PartialEq, Insertable)]
+    #[diesel(table_name = news)]
+    #[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+    pub struct NewsInput {
+        title: String,
+        pub_date: NaiveDateTime,
+        sources: Sources,
+    }
+    
 }
 
 mod repository {
-    use super::*;
+    use anyhow::Result;
+    use diesel::prelude::*;
+    use crate::schema::news;
+    use deadpool_diesel::sqlite::Pool;
+    use super::model::{News, NewsInput};
 
     pub(super) async fn list(pool: &Pool, days_ago: u8) -> Result<Vec<News>, Box<dyn std::error::Error>> {
 
@@ -98,6 +99,13 @@ mod repository {
     
 }
 
+use crate::app::AppState;
+use anyhow::Result;
+use axum::extract::Path;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::Json;
+use model::{News, NewsInput};
 
 // get news list handler
 pub async fn list(State(state): State<AppState>, days_ago: Option<Path<u8>>) -> Result<Json<Vec<News>>, StatusCode> {
