854ac7cba6e8501e457bcd0fec4a31acdf0b4933
esprit <espritf@users.noreply.github.com>
2024-02-26 01:49:22 +0200

    server: post news through api

--- server/src/publisher.rs
+++ server/src/publisher.rs
@@ -1,28 +1,33 @@
-use crate::schema::{channels, items, news};
+use crate::schema::{channels, items};
 use anyhow::Result;
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use serde::Serialize;
 use crate::translator;
 use serde_json;
-use serde_json::json;
+use serde_json::Value;
 
-#[derive(Insertable)]
-#[diesel(table_name = news)]
+#[derive(Serialize)]
 pub struct News {
-    sources: String,
+    sources: Vec<String>,
     title: String,
     pub_date: NaiveDateTime,
 }
 
-// grab all unpublished news, translate them and publish
-pub fn publish(conn: &mut SqliteConnection) -> Result<()> {
-    type Item = (i32, String, String, NaiveDateTime, String);
+type Item = (i32, String, String, NaiveDateTime, String);
 
-    let items: Vec<Item> = items::table
+fn not_published(conn: &mut SqliteConnection) -> QueryResult<Vec<Item>> {
+    items::table
         .inner_join(channels::table)
         .filter(items::published_id.is_null())
         .select((items::id, items::link, items::title, items::pub_date, channels::language))
-        .load(conn)?;
+        .load(conn)
+}
+
+// grab all unpublished news, translate them and publish
+pub fn publish(conn: &mut SqliteConnection) -> Result<()> {
+
+    let items = not_published(conn)?;
 
     for (id, link, title, pub_date, lang) in items {
 
@@ -34,25 +39,30 @@ pub fn publish(conn: &mut SqliteConnection) -> Result<()> {
         };
 
         let publication = News {
-            sources: json!([link]).to_string(),
+            sources: vec![link],
             title,
             pub_date,
         };
+        
+        // get nedpoint from env variable
+        let endpoint = std::env::var("NEWS_API_ENDPOINT")?;
+        let response = reqwest::blocking::Client::new()
+            .post(endpoint)
+            .json(&publication)
+            .send()?;
+        
+        let json = response.json::<Value>()?;
+        
+        // get id from json
+        let published_id = json
+            .get("id").ok_or(anyhow::anyhow!("No id in response"))?
+            .as_u64().ok_or(anyhow::anyhow!("Id is not a number"))?;
+        
+        tracing::info!("Item {} publishded with id: {}", &publication.title, &published_id);
 
-        conn.transaction(|c| -> Result<()> {
-            tracing::info!("Publish news with title: {}", publication.title);
-
-            let published_id = diesel::insert_into(news::table)
-                .values(&publication)
-                .returning(news::id)
-                .get_result::<i32>(c)?;
-            
-            diesel::update(items::table.filter(items::id.eq(id)))
-                .set(items::published_id.eq(published_id))
-                .execute(c)?;
-
-            Ok(())
-        })?;
+        diesel::update(items::table.filter(items::id.eq(id)))
+            .set(items::published_id.eq(published_id as i32)) // Why i32?
+            .execute(conn)?;
     }
 
     Ok(())
