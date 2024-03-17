e3db8fe0ff0d1943cdfd7e582f3a75a66c6f345a
esprit <espritf@users.noreply.github.com>
2024-03-17 18:14:43 +0200

    server: extract pool creation function

--- server/src/app.rs
+++ server/src/app.rs
@@ -1,5 +1,12 @@
+use crate::news::handlers::NewsRepository;
 use anyhow::Result;
 use deadpool_diesel::sqlite::{Manager, Pool, Runtime};
+
+fn pool(uri: &String) -> Result<Pool> {
+    let manager = Manager::new(uri, Runtime::Tokio1);
+    let pool = Pool::builder(manager).build()?;
+    Ok(pool)
+}
 
 pub trait Repository {
     fn new(pool: Pool) -> Self;
@@ -12,13 +19,14 @@ pub struct AppState {
 
 impl AppState {
     pub fn build(uri: &String) -> Result<Self> {
-        let manager = Manager::new(uri, Runtime::Tokio1);
-        let pool = Pool::builder(manager).build()?;
-
+        let pool = pool(uri)?;
         Ok(AppState { pool })
     }
 
-    pub fn get_repo<T: Repository>(&self) -> T {
+    pub fn get_repo<T>(&self) -> T
+    where
+        T: Repository + NewsRepository,
+    {
         T::new(self.pool.clone())
     }
 }
