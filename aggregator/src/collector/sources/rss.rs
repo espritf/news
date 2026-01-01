79131d31d712ddabc894ca2fd600c7dbd1a4c40f
esprit <espritf@users.noreply.github.com>
2026-01-02 00:01:56 +0200

    aggregator: details argument for required

--- aggregator/src/collector/sources/rss.rs
+++ aggregator/src/collector/sources/rss.rs
@@ -15,12 +15,12 @@ pub fn fetch(config: &Config) -> Result<Data> {
     let res = reqwest::blocking::get(&config.url)?.text()?;
     let ch = rss::Channel::read_from(res.as_bytes())?;
 
-    let last_build_date = DateTime::parse_from_rfc2822(ch.last_build_date().required()?)?;
+    let last_build_date = DateTime::parse_from_rfc2822(ch.last_build_date().required("channel last_build_date")?)?;
 
     let channel = Channel {
         title: ch.title().to_owned(),
         link: config.url.as_str().to_owned(),
-        language: ch.language().required()?.to_owned(),
+        language: ch.language().required("channel language")?.to_owned(),
         last_build_date: Some(last_build_date.naive_local()),
     };
 
@@ -28,13 +28,13 @@ pub fn fetch(config: &Config) -> Result<Data> {
         .items()
         .iter()
         .map(|i| {
-            let pub_date = DateTime::parse_from_rfc2822(i.pub_date().required()?)?;
+            let pub_date = DateTime::parse_from_rfc2822(i.pub_date().required("item pub_date")?)?;
             let tags: Vec<&str> = i.categories().iter().map(|c| c.name()).collect();
 
             let item = Item {
-                guid: i.guid().required()?.value().to_owned(),
-                title: i.title().required()?.to_owned(),
-                link: i.link().required()?.to_owned(),
+                guid: i.guid().required("item guid")?.value().to_owned(),
+                title: i.title().required("item title")?.to_owned(),
+                link: i.link().required("item link")?.to_owned(),
                 pub_date: pub_date.naive_local().to_owned(),
                 tags: Some(serde_json::to_string(&tags)?),
             };
--- aggregator/src/error.rs
+++ aggregator/src/error.rs
@@ -1,12 +1,12 @@
 use anyhow::{Result, Error};
 
 pub trait IsRequired<T> {
-    fn required(self) -> Result<T>;
+    fn required(self, details: &str) -> Result<T>;
 }
 
 impl<T> IsRequired<T> for Option<T> {
-    fn required(self) -> Result<T> {
-        self.ok_or("required data is missing").map_err(Error::msg)
+    fn required(self, details: &str) -> Result<T> {
+        self.ok_or(format!("required data is missing: {}", details)).map_err(Error::msg)
     }
 }
 
